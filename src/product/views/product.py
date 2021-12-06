e1c8d274bbd4c7f2caf2e6706ad915809f38c535
Mehedi Hasan Bappi <mehedibubt34@gmail.com>
2021-12-06 15:44:49 +0600

    product search list compeleted

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.views import generic
 from django.views.generic.list import ListView
-from product.models import Variant, Product, ProductVariant
+from product.models import Variant, Product, ProductVariant, ProductVariantPrice
 
 
 class CreateProductView(generic.TemplateView):
@@ -30,5 +31,26 @@ class ProductList(ListView):
 
 
 def ProductSearch(request):
-    print(request.POST)
-    return render(request, 'products/search-result.html', {})
+    data = {}
+    if request.method == 'POST':
+        data['title'] = request.POST['title']
+        data['variant'] = request.POST['variant']
+        data['price_from'] = request.POST['price_from']
+        data['price_to'] = request.POST['price_to']
+
+        product = Product.objects.all()
+        if data['title']:
+            product = product.filter(title__contains=data['title'])
+        product = ProductVariantPrice.objects.filter(product__in=product)
+
+        if data['variant']:
+            product = product.filter(Q(product_variant_one__variant_title=data['variant']) | 
+                                                    Q(product_variant_two__variant_title=data['variant']) | 
+                                                    Q(product_variant_three__variant_title=data['variant']))
+
+        if data['price_from'] or data['price_to']:
+            start = int(data['price_from']) if data['price_from'] else 0
+            end = int(data['price_to']) if data['price_to'] else 0
+            product = product.filter(price__range=(start, end))
+            
+    return render(request, 'products/search-result.html', {'products': product})
