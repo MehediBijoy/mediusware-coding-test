0764d3b21b3d48c015e88e594532d9bb0afcfe62
MehediBijoy <mehedibubt34@gmail.com>
2021-12-10 00:25:53 +0600

    Product filter update

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,3 +1,4 @@
+import sys
 from django.forms.models import inlineformset_factory
 from django.shortcuts import redirect, render
 from django.db.models import Q
@@ -41,30 +42,31 @@ def ProductSearch(request):
         data['price_from'] = request.POST['price_from']
         data['price_to'] = request.POST['price_to']
 
-        product = Product.objects.all()
-        if data['title']:
-            product = product.filter(title__contains=data['title'])
-        product = ProductVariantPrice.objects.filter(product__in=product)
+        if data['title'] or data['variant'] or data['price_from'] or data['price_to']:
+            product = Product.objects.all()
+            if data['title']:
+                product = product.filter(title__contains=data['title'])
 
-        if data['variant']:
-            product = product.filter(Q(product_variant_one__variant_title=data['variant']) | 
-                                                    Q(product_variant_two__variant_title=data['variant']) | 
-                                                    Q(product_variant_three__variant_title=data['variant']))
+            if data['variant']:
+                product = product.filter(Q(product_variant__product_variant_one__variant_title=data['variant']) | 
+                                        Q(product_variant__product_variant_two__variant_title=data['variant']) | 
+                                        Q(product_variant__product_variant_three__variant_title=data['variant'])).distinct()
 
-        if data['price_from'] or data['price_to']:
-            start = int(data['price_from']) if data['price_from'] else 0
-            end = int(data['price_to']) if data['price_to'] else 0
-            product = product.filter(price__range=(start, end))
-            
-    return render(request, 'products/search-result.html', {'products': product})
+            if data['price_from'] or data['price_to']:
+                start = int(data['price_from']) if data['price_from'] else 0
+                end = int(data['price_to']) if data['price_to'] else sys.maxsize
+                product = product.filter(product_variant__price__range=(start, end)).distinct()
 
+            return render(request, 'products/search-result.html', {'products': product})
+        else:
+            return render(request, 'products/search-result.html', {'products': None})
 
 def updateProduct(request, id):
     context = {}
     product = Product.objects.get(id=id)
     formset = inlineformset_factory(Product, ProductVariantPrice, 
             fields=('product_variant_one', 'product_variant_two', 'product_variant_three', 'price', 'stock'), 
-            extra=0, can_delete=False)
+            extra=1, can_delete=True)
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
